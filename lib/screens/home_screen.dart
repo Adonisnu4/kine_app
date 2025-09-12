6e4a90f200220cc1d6de65d9d71ed0774b1115a0
Adonisnu4 <adonisnunez5@gmail.com>
2025-09-11 22:10:43 -0300

    home_screen hecho

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 
+//import screens
+import 'package:kine_app/screens/exercises_screen.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            "Hola", style: TextStyle(color: Colors.white, fontSize: 24)
-          ),
+    return DefaultTabController(
+      length: 3, // Número de pestañas
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Mi App con Pestañas'),
+          automaticallyImplyLeading: false, // Oculta el botón de "atrás"
         ),
-      )
+        
+        // Aquí se define el contenido de cada pestaña
+        body: const TabBarView(
+          children: [
+            Center(child: Text('Contenido de Inicio')),
+            ExercisesScreen(),
+            Center(child: Text('Contenido de Perfil')),
+          ],
+        ),
+        
+        // Esta es la clave: el TabBar se mueve a la navegación inferior
+        bottomNavigationBar: const TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.home), text: 'Inicio'),
+            Tab(icon: Icon(Icons.search), text: 'Ejercicios'),
+            Tab(icon: Icon(Icons.person), text: 'Perfil'),
+          ],
+        ),
+      ),
     );
   }
 }
